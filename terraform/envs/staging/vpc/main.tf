559ae8af69be1a9bd1588e2b8280a7fab10428ec
Raymond Usbal <raymond@philippinedev.com>
2022-05-27 07:50:46 +0800

    added private subnets

--- terraform/envs/staging/vpc/main.tf
+++ terraform/envs/staging/vpc/main.tf
@@ -22,3 +22,46 @@ module "terraform-aws-vpc" {
   vpc_name        = local.vpc_name
 
 }
+
+resource "aws_subnet" "subnet_web_1" {
+  vpc_id            = module.terraform-aws-vpc.vpc_id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-southeast-1a"
+  tags              = local.tags
+}
+
+resource "aws_subnet" "subnet_web_2" {
+  vpc_id            = module.terraform-aws-vpc.vpc_id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-southeast-1b"
+  tags              = local.tags
+}
+
+resource "aws_subnet" "subnet_app_1" {
+  vpc_id            = module.terraform-aws-vpc.vpc_id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "ap-southeast-1a"
+  tags              = local.tags
+}
+
+resource "aws_subnet" "subnet_app_2" {
+  vpc_id            = module.terraform-aws-vpc.vpc_id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "ap-southeast-1b"
+  tags              = local.tags
+}
+
+resource "aws_subnet" "subnet_db_1" {
+  vpc_id            = module.terraform-aws-vpc.vpc_id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "ap-southeast-1a"
+  tags              = local.tags
+}
+
+resource "aws_subnet" "subnet_db_2" {
+  vpc_id            = module.terraform-aws-vpc.vpc_id
+  cidr_block        = "10.0.7.0/24"
+  availability_zone = "ap-southeast-1b"
+  tags              = local.tags
+}
+

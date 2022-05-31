2361319fd9336783ed6e05b8692c0d497de4c003
Raymond Usbal <raymond@philippinedev.com>
2022-05-31 23:22:31 +0800

    route tables private

--- terraform/envs/staging/vpc/route_table.tf
+++ terraform/envs/staging/vpc/route_table.tf
@@ -1,41 +1,39 @@
 resource "aws_route_table" "priv_rt_one" {
   vpc_id = module.myvpc.vpc_id
-
-  route {
-    nat_gateway_id = aws_nat_gateway.nat1.id
-    cidr_block = "10.0.2.0/24"
-  }
-
-  route {
-    nat_gateway_id = aws_nat_gateway.nat1.id
-    cidr_block = "10.0.4.0/24"
-  }
-
-  route {
-    nat_gateway_id = aws_nat_gateway.nat1.id
-    cidr_block = "10.0.6.0/24"
-  }
-
-  tags = merge(local.tags, { Name = "AZ-one RT" })
+  tags = merge(local.tags, { Name = "Private RT1" })
 }
 
 resource "aws_route_table" "priv_rt_two" {
   vpc_id = module.myvpc.vpc_id
+  tags = merge(local.tags, { Name = "Private RT2" })
+}
 
-  route {
-    nat_gateway_id = aws_nat_gateway.nat2.id
-    cidr_block = "10.0.3.0/24"
-  }
+resource "aws_route_table_association" "a1" {
+  subnet_id      = aws_subnet.subnet_web_1.id
+  route_table_id = aws_route_table.priv_rt_one.id
+}
 
-  route {
-    nat_gateway_id = aws_nat_gateway.nat2.id
-    cidr_block = "10.0.5.0/24"
-  }
+resource "aws_route_table_association" "a2" {
+  subnet_id      = aws_subnet.subnet_web_2.id
+  route_table_id = aws_route_table.priv_rt_two.id
+}
 
-  route {
-    nat_gateway_id = aws_nat_gateway.nat2.id
-    cidr_block = "10.0.7.0/24"
-  }
+resource "aws_route_table_association" "b1" {
+  subnet_id      = aws_subnet.subnet_app_1.id
+  route_table_id = aws_route_table.priv_rt_one.id
+}
 
-  tags = merge(local.tags, { Name = "AZ-two RT" })
+resource "aws_route_table_association" "b2" {
+  subnet_id      = aws_subnet.subnet_app_2.id
+  route_table_id = aws_route_table.priv_rt_two.id
+}
+
+resource "aws_route_table_association" "c1" {
+  subnet_id      = aws_subnet.subnet_db_1.id
+  route_table_id = aws_route_table.priv_rt_one.id
+}
+
+resource "aws_route_table_association" "c2" {
+  subnet_id      = aws_subnet.subnet_db_2.id
+  route_table_id = aws_route_table.priv_rt_two.id
 }

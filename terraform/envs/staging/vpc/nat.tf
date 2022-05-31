16c9b6ded535e3fbe97e451187ecc22673573e9e
Raymond Usbal <raymond@philippinedev.com>
2022-05-31 22:13:35 +0800

    subnet names

--- terraform/envs/staging/vpc/nat.tf
+++ terraform/envs/staging/vpc/nat.tf
@@ -8,14 +8,14 @@ resource "aws_eip" "eip1" {
 }
 
 resource "aws_eip" "eip2" {
-  vpc               = true
-  tags              = local.tags
+  vpc  = true
+  tags = local.tags
 }
 
 resource "aws_nat_gateway" "nat1" {
   allocation_id = aws_eip.eip1.id
   subnet_id     = module.myvpc.subnet_ids[0]
-  tags = local.tags
+  tags          = local.tags
 }
 
 resource "aws_nat_gateway" "nat2" {
--- terraform/envs/staging/vpc/subnets.tf
+++ terraform/envs/staging/vpc/subnets.tf
@@ -6,40 +6,52 @@ resource "aws_subnet" "subnet_web_1" {
   vpc_id            = module.myvpc.vpc_id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-southeast-1a"
-  tags              = local.private_tags
+  tags = merge(local.private_tags, {
+    Name = "PRIVATE Subnet Web 1"
+  })
 }
 
 resource "aws_subnet" "subnet_web_2" {
   vpc_id            = module.myvpc.vpc_id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-southeast-1b"
-  tags              = local.private_tags
+  tags = merge(local.private_tags, {
+    Name = "PRIVATE Subnet Web 2"
+  })
 }
 
 resource "aws_subnet" "subnet_app_1" {
   vpc_id            = module.myvpc.vpc_id
   cidr_block        = "10.0.4.0/24"
   availability_zone = "ap-southeast-1a"
-  tags              = local.private_tags
+  tags = merge(local.private_tags, {
+    Name = "PRIVATE Subnet App 1"
+  })
 }
 
 resource "aws_subnet" "subnet_app_2" {
   vpc_id            = module.myvpc.vpc_id
   cidr_block        = "10.0.5.0/24"
   availability_zone = "ap-southeast-1b"
-  tags              = local.private_tags
+  tags = merge(local.private_tags, {
+    Name = "PRIVATE Subnet App 2"
+  })
 }
 
 resource "aws_subnet" "subnet_db_1" {
   vpc_id            = module.myvpc.vpc_id
   cidr_block        = "10.0.6.0/24"
   availability_zone = "ap-southeast-1a"
-  tags              = local.private_tags
+  tags = merge(local.private_tags, {
+    Name = "PRIVATE Subnet DB 1"
+  })
 }
 
 resource "aws_subnet" "subnet_db_2" {
   vpc_id            = module.myvpc.vpc_id
   cidr_block        = "10.0.7.0/24"
   availability_zone = "ap-southeast-1b"
-  tags              = local.private_tags
+  tags = merge(local.private_tags, {
+    Name = "PRIVATE Subnet DB 2"
+  })
 }

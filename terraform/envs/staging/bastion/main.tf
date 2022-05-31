3f30bd83eb10549b1571677bfda9a4dfc2e68ef8
Raymond Usbal <raymond@philippinedev.com>
2022-05-31 16:21:37 +0800

    bastion 1

--- terraform/envs/staging/bastion/main.tf
+++ terraform/envs/staging/bastion/main.tf
@@ -1,25 +1,36 @@
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "ssh-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdHCU5aOgHxVOwwrJzOJ0NZxk9wy//BPlFp63sfNO4pFfkhoRJqVOSD+oQ/OxBl1+9aVAIzNiFGVwBXrRrYzyQZ1N5/3DxEC3mlN+JugMGJIPm+MfLBb1CWVXCaSAuJjWfgUNq8K1d0YIxbZmg6cJJ/7h83HD6xi3qXwOUsmKDeNebrp01i2Ef8168zBYqjethnzjbqyGHEIppCCGJxrEKpqMNvSK9kpGNTx6wkseeH/30Evofp9DbBLucx1K+HWALWLxUrL4wQ46qnZo8ViEE/Aif4Y1Z0oz6WStXw1S9PD5UQHDiF7p/I5CbMOW+yV5aGZiBbE4aDRhTRFMLqgaqn5ZV4jokvSHBn+AL89yEP2UbGcXAKrK/GlYjEgBYRTRFQzesPl5FdJe+oK6e/sOD5noqrryzEDmv+TwZOg/gKv0bdBAvDt19VP5lK/s3SILmWWGdoYhrtUnH1G2GUqCSSJf0V5pdp/dLiOo5XbnUfmXESS+kcR+JHNd+Aysx2pU= rsusbal@Raymonds-iMac-2.local"
-
-  tags = {
-    Name = "lisadaly"
+locals {
+  raymond_key = "ssh-key"
+  tags        = {
+    Name = "lisa_daly"
   }
 }
 
-module "bastion" {
-  source  = "umotif-public/bastion/aws"
-  version = "~> 2.1.0"
+resource "aws_key_pair" "ssh-key" {
+  key_name   = local.raymond_key
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCdHCU5aOgHxVOwwrJzOJ0NZxk9wy//BPlFp63sfNO4pFfkhoRJqVOSD+oQ/OxBl1+9aVAIzNiFGVwBXrRrYzyQZ1N5/3DxEC3mlN+JugMGJIPm+MfLBb1CWVXCaSAuJjWfgUNq8K1d0YIxbZmg6cJJ/7h83HD6xi3qXwOUsmKDeNebrp01i2Ef8168zBYqjethnzjbqyGHEIppCCGJxrEKpqMNvSK9kpGNTx6wkseeH/30Evofp9DbBLucx1K+HWALWLxUrL4wQ46qnZo8ViEE/Aif4Y1Z0oz6WStXw1S9PD5UQHDiF7p/I5CbMOW+yV5aGZiBbE4aDRhTRFMLqgaqn5ZV4jokvSHBn+AL89yEP2UbGcXAKrK/GlYjEgBYRTRFQzesPl5FdJe+oK6e/sOD5noqrryzEDmv+TwZOg/gKv0bdBAvDt19VP5lK/s3SILmWWGdoYhrtUnH1G2GUqCSSJf0V5pdp/dLiOo5XbnUfmXESS+kcR+JHNd+Aysx2pU= rsusbal@Raymonds-iMac-2.local"
 
-  name_prefix = "lisadaly"
+  tags = local.tags
+}
 
-  vpc_id         = data.terraform_remote_state.vpc.outputs.vpc_id
-  public_subnets = data.terraform_remote_state.vpc.outputs.subnet_ids
-  region         = "ap-southeast-1"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
 
-  ssh_key_name = aws_key_pair.ssh-key.key_name
-
-  tags = {
-    Name = "lisadaly"
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.nano"
+  key_name      = local.raymond_key
+
+  tags = local.tags
 }
